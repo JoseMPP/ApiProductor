9ca5195d6b2bf2a3824a666804af59488e9dec65
Jose Pizarro <josmapizarro.99@gmail.com>
2021-04-20 17:32:03 -0400

    Agregar archivos de proyecto.

---
+++ ApiProductor/Models/Data.cs
@@ -0,0 +1,20 @@
+﻿namespace ApiProductor.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    public class Data
+    {
+        [Key]
+        public string NameDevice { get; set; }
+        [DataType(DataType.DateTime)]
+        [Required]
+        public DateTime EventDate { get; set; }
+        [Required]
+        public string Temperatura { get; set; }
+        [Required]
+        public string Humedad { get; set; }
+        
+            
+    }
+
+}
